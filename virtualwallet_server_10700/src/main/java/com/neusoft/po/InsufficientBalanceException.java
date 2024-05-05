c77110d1fb641d27f6b461f863f678397b2e627c
AlanZeng423 <alanzeng423@gmail.com>
2024-05-05 16:20:31 +0800

    Final Commit & add Report

--- virtualwallet_server_10700/src/main/java/com/neusoft/po/InsufficientBalanceException.java
+++
@@ -1,8 +0,0 @@
-package com.neusoft.po;
-
-public class InsufficientBalanceException extends Exception {
-	public InsufficientBalanceException(String massage) {
-		super(massage);
-	}
-
-}
--- virtualwallet_server_10701/src/main/java/com/neusoft/po/InvalidAmountException.java
+++
@@ -1,7 +0,0 @@
-package com.neusoft.po;
-
-public class InvalidAmountException extends Exception {
-	public InvalidAmountException(String massage) {
-		super(massage);
-	}
-}
